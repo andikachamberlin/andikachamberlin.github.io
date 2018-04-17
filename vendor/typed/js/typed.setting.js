2962336ef10af15d1649235a4ef4f32cf79612f9
Andika Chamberlin <htmlandika@gmail.com>
2018-04-17 14:03:35 +0700

    Add files via upload

---
+++ vendor/typed/js/typed.setting.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var typed = new Typed('#typed', {
+    strings: ['Front End' , 'Developer.'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true
+  });
+
+});
