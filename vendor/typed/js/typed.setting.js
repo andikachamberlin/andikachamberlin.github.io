37016d1acf4e24c9c038a561ad54307e6cbc7523
Andika Chamberlin <htmlandika@gmail.com>
2018-10-01 13:51:25 +0700

    Update typed.setting.js

--- vendor/typed/js/typed.setting.js
+++ vendor/typed/js/typed.setting.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   
   var typed = new Typed('#typed', {
-    strings: ['Front End' , 'Back End', 'Blogger', 'Developer'],
+    strings: ['Front End', 'Back End', 'Wordpress', 'Blogger', 'Developer'],
     typeSpeed: 100,
     backSpeed: 50,
     smartBackspace: true,
